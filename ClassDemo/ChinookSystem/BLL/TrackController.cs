2de8611e43afb69aacbcff44dc5ccc70750178b1
Brandon J Whiteford <bwhiteford2@nait.ca>
2017-11-08 20:50:27 -0700

    Added code to manage playlist (utilizing controls behind)

--- ClassDemo/ChinookSystem/BLL/TrackController.cs
+++ ClassDemo/ChinookSystem/BLL/TrackController.cs
@@ -17,19 +17,112 @@ namespace ChinookSystem.BLL
     [DataObject]
     public class TrackController
     {
-        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
         public List<TrackList> List_TracksForPlaylistSelection(string tracksby, int argid)
         {
             using (var context = new ChinookContext())
             {
-                List<TrackList> results = null;
+                IEnumerable<TrackList> results = null;
 
-               //code to go here
+                //code to go here
+                // determine which lookup needs to be done : tracksby
+                switch (tracksby)
+                {
+                    case "Artist":
+                        {
+                            results = from x in context.Tracks
+                                      orderby x.Name
+                                      where x.Album.ArtistId == argid
+                                      select new TrackList
+                                      {
+                                          TrackID = x.TrackId,
+                                          Name = x.Name,
+                                          Title = x.Album.Title,
+                                          MediaName = x.MediaType.Name,
+                                          GenreName = x.Genre.Name,
+                                          Composer = x.Composer,
+                                          Milliseconds = x.Milliseconds,
+                                          Bytes = x.Bytes,
+                                          UnitPrice = x.UnitPrice
+                                      };
+                            break;
+                        }
+                    case "MediaType":
+                        {
+                            results = from x in context.Tracks
+                                      orderby x.Name
+                                      where x.MediaTypeId == argid
+                                      select new TrackList
+                                      {
+                                          TrackID = x.TrackId,
+                                          Name = x.Name,
+                                          Title = x.Album.Title,
+                                          MediaName = x.MediaType.Name,
+                                          GenreName = x.Genre.Name,
+                                          Composer = x.Composer,
+                                          Milliseconds = x.Milliseconds,
+                                          Bytes = x.Bytes,
+                                          UnitPrice = x.UnitPrice
+                                      };
+                            break;
+                        }
+                    case "Genre":
+                        {
+                            results = from x in context.Tracks
+                                      orderby x.Name
+                                      where x.GenreId == argid
+                                      select new TrackList
+                                      {
+                                          TrackID = x.TrackId,
+                                          Name = x.Name,
+                                          Title = x.Album.Title,
+                                          MediaName = x.MediaType.Name,
+                                          GenreName = x.Genre.Name,
+                                          Composer = x.Composer,
+                                          Milliseconds = x.Milliseconds,
+                                          Bytes = x.Bytes,
+                                          UnitPrice = x.UnitPrice
+                                      };
+                            break;
+                        }
 
-                return results;
+                    default:
+                        {
+                            results = from x in context.Tracks
+                                      orderby x.Name
+                                      where x.AlbumId == argid
+                                      select new TrackList
+                                      {
+                                          TrackID = x.TrackId,
+                                          Name = x.Name,
+                                          Title = x.Album.Title,
+                                          MediaName = x.MediaType.Name,
+                                          GenreName = x.Genre.Name,
+                                          Composer = x.Composer,
+                                          Milliseconds = x.Milliseconds,
+                                          Bytes = x.Bytes,
+                                          UnitPrice = x.UnitPrice
+                                      };
+                            break;
+                        }
+
+
+                     /*
+                     results = from x in context.Tracks
+                     orderby x.Name 
+                     where tracksby.Equals("Artist") ? x.Album.ArtistId == argid : 
+                     tracksby.Equals("MediaType") ? x.MediaTypeId == argid : 
+                     tracksby.Equals("Genre") ? x.GenreId == argid :
+                     x.AlbumId == argid
+                         
+                     */
+
+                }
+                return results.ToList();
             }
+
         }//eom
 
-       
+
     }//eoc
 }
