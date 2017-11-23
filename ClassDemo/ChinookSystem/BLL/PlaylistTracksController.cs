bce51f7080bd50b291823590d54e01d630187a83
Brandon J Whiteford <bwhiteford2@nait.ca>
2017-11-22 20:21:58 -0700

    Added x button to Manage Playlist

--- ClassDemo/ChinookSystem/BLL/PlaylistTracksController.cs
+++ ClassDemo/ChinookSystem/BLL/PlaylistTracksController.cs
@@ -193,8 +193,38 @@ namespace ChinookSystem.BLL
             using (var context = new ChinookContext())
             {
                 //code to go here
-
-
+                var exists = (from x in context.Playlists
+                              where x.UserName.Equals(username) && x.Name.Equals(playlistname)
+                              select x).FirstOrDefault();
+                if (exists == null)
+                {
+                    throw new Exception("Playlist has been removed from the file.");
+                }
+                else
+                {
+                    // find tracks that will be kept
+                    var tracksKept = exists.PlaylistTracks.Where(tr => !trackstodelete.Any(tod => tod == tr.TrackId)).Select(tr => tr);
+                    // remove unwanted tracks
+                    PlaylistTrack item = null;
+                    foreach (var dtrackid in trackstodelete)
+                    {
+                        item = exists.PlaylistTracks.Where(tr => tr.TrackId == dtrackid).FirstOrDefault();
+                        if (item != null)
+                        {                            
+                            exists.PlaylistTracks.Remove(item);
+                        }
+                        
+                    }
+                    // renumber remaining kept list
+                    int number = 1;
+                    foreach (var tkept in tracksKept)
+                    {
+                        tkept.TrackNumber = number;
+                        context.Entry(tkept).Property(y => y.TrackNumber).IsModified = true;
+                        number++;
+                    }
+                    context.SaveChanges();
+                }
             }
         }//eom
     }
